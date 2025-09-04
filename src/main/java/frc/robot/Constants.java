fd313b4666eb5c1886932a8aee91e7a0a2ba0400
pedrohjsilva011 <pdejesussilva07@gmail.com>
2025-09-04 13:21:10 -0300

    Incorporado sistema dos braços
    Já funcionando

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,6 +8,7 @@ public final class Constants {
   public static int Encoder_A = 0;
   public static int Encoder_B = 1;
   public static int controller = 0;
+  public static int controller1 = 1;
   public static double deadZone = 0.04;
   public static int L_Trigger = 2;
   public static int R_Trigger = 3;
@@ -17,4 +18,7 @@ public final class Constants {
   public static int button_X = 3;
   public static int right_X = 4;
   public static int right_Y = 5;
+  public static final int kArmMotorCanId = 5;
+  public static final double kArmUpSpeed = 0.1;
+  public static final double kArmDownSpeed = -0.07;
 }
